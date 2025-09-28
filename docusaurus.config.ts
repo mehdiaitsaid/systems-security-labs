a8e30e0998b0f04c1346ce50b43a71363f98d53c
mehdiaitsaid <aitsaid.mahdi@gmail.com>
2025-09-28 22:01:16 +0100

    Add GitHub Action for Docusaurus deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,6 @@ const config: Config = {
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
-
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
     organizationName: 'mehdiaitsaid', // Usually your GitHub org/user name.
